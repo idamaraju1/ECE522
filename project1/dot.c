fcef0affac19e0cbb2fa0aa0d1c4ef8fb4cabdfb
Iresh Damaraju <idamaraju@wisc.edu>
2025-09-25 18:02:50 -0500

    project2 template

--- project1/dot.c
+++ project1/dot.c
@@ -9,5 +9,9 @@ extern uint32_t umul(const uint32_t x, const uint32_t y);
 uint32_t dot(const uint32_t *const A, const uint32_t *const B, const size_t len)
 {
     // Replace with your implementation of dot product.
-    return 0;
+    uint32_t result = 0;
+    for (size_t i = 0; i < len; i++) {
+        result += umul(A[i], B[i]);
+    }
+    return result;
 }
--- project1/umul.s
+++ project1/umul.s
@@ -1,4 +1,4 @@
-## Author: Your Name
+## Author: Iresh Damaraju
 ##
 ## You may implement the following with any of the instructions in the RV32I instruction set
 ## and described in the reference sheet. Do not use any of the mul[h][s][u] instructions which
@@ -16,10 +16,23 @@
 ##
 ## [Returns]
 ## a0 = 32-bit product
-    .text
-    .globl umul
+
+.text
+.globl umul
 umul:
-    # This dummy code adds the two operands and returns the result.
-    # Replace with your implementation.
-    add  a0, a0, a1
-    jalr zero, 0(ra)
+
+    addi    t0, x0, 0       # res = 0
+    addi    t1, a1, 0       # t1 = multiplier (b)
+    addi    t2, a0, 0       # t2 = multiplicand (a)
+
+    beq     t1, x0, 1f      # if b==0, return 0
+
+0:  andi    t3, t1, 1       # t3 = b & 1
+    beq     t3, x0, 2f      # if LSB==0, skip add
+    add     t0, t0, t2      # res += a (mod 2^32)
+2:  slli    t2, t2, 1       # a <<= 1
+    srli    t1, t1, 1       # b >>= 1
+    bne     t1, x0, 0b      # loop while b != 0
+
+1:  addi    a0, t0, 0       # move result to a0 (no 'mv' pseudo)
+    jalr    x0, ra, 0       # return (no 'ret' pseudo)
